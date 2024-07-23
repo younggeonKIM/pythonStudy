6b38af74eff55081f9d93ef36dc19194403e52d7
영건 김 <young3702@naver.com>
2024-07-23 21:11:41 +0900

    20240722_pythonStudy(inputFunction, typeConversion)_added

---
+++ helloWorld/convertingType.py
@@ -0,0 +1,8 @@
+birth_year = input("당신의 태어난 연도를 입력하시오. ")
+age = 2020 - int(birth_year)
+print(age)
+
+first_value = input("First: ")
+second_value = input("Second: ")
+calculated_value = float(first_value) + float(second_value)
+print("Sum: " + str(calculated_value))
