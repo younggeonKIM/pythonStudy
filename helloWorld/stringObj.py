9e4530afd54be3f943ad3cbb817556dd1c99c1b7
영건 김 <young3702@naver.com>
2024-07-24 20:55:07 +0900

    20240724_pythonStudy(strObj)_added

---
+++ helloWorld/stringObj.py
@@ -0,0 +1,9 @@
+course = 'Python for Beginners'
+print(course.upper())
+print(course.find('y'))
+print(course.find('for'))
+print(course.find('Y'))
+print(course.replace('for', '4'))
+print(course.replace('x', '4'))
+print('Python' in course)
+print(course)
