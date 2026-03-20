8aa972dc986dcd2a99f89be13774d0ae20280d54
영건 김 <young3702@naver.com>
2026-03-20 21:32:41 +0900

    20260320_stringStudy_modified2

--- helloWorld/pythonBasic/stringObj.py
+++ helloWorld/pythonBasic/stringObj.py
@@ -11,3 +11,7 @@ print(course)
 name = """Kim Younggeon"""
 print(name)
 print(name.upper())
+
+#작은 따옴표 안에 있는 큰 따옴표는 문자열로 인식되지 않음
+dialogue = '"Whatever my lot, it is well with my soul" said Oh'
+print(dialogue)
