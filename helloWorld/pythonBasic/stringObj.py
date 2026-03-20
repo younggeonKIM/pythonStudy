f776bc0de6ead8a16cc0674e2f37e025d3befc2c
영건 김 <young3702@naver.com>
2026-03-20 21:37:02 +0900

    20260320_stringStudy_modified3

--- helloWorld/pythonBasic/stringObj.py
+++ helloWorld/pythonBasic/stringObj.py
@@ -15,3 +15,8 @@ print(name.upper())
 #작은 따옴표 안에 있는 큰 따옴표는 문자열로 인식되지 않음
 dialogue = '"Whatever my lot, it is well with my soul" said Oh'
 print(dialogue)
+
+
+#이스케이프 문자인 역슬래시(\) 를 이용해 문자열 내에 작은따옴표나 큰따옴표를 포함시킬 수 있음
+anniversary = 'NY\'s birthday'
+print(anniversary + ' is Feb 4th')
