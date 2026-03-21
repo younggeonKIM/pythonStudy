c6b789ec913384289606961f3e72ab7a3937686c
영건 김 <young3702@naver.com>
2026-03-21 23:46:36 +0900

    20260321_stringFormattingStudy_modified

--- helloWorld/pythonBasic/stringFormatting.py
+++ helloWorld/pythonBasic/stringFormatting.py
@@ -8,3 +8,22 @@ print(a)
 number=10
 b="I want %d axes." %number
 print(b)
+
+
+money=10000
+feeling='happy'
+c="I have %d wons, I was %s in all that day." %(money, feeling)
+print(c)
+
+"""
+rate=10
+d='This products have %d% of defect rate.' %rate    ---> 문자열 포맷 코드 %d를 사용할 때는 %를 
+                                                        그냥 사용할 수 없고 %% 라는 문자열 포맷
+                                                        코드를 또 사용해야 %를 그대로 사용할 수
+                                                        있음
+print(d)
+"""
+
+rate=10
+d='This products have %d%% of defect rates.' %rate
+print(d)
