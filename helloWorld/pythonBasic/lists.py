2f32042e78abce95ce5e7700c6bd682bb460d232
영건 김 <young3702@naver.com>
2026-07-25 17:00:54 +0900

    20260725_listStudy_modified

--- helloWorld/pythonBasic/lists.py
+++ helloWorld/pythonBasic/lists.py
@@ -9,5 +9,12 @@ print(names[0:3])
 
 odd = [1, 3, 5, 7, 9]
 print(odd)
-print(odd[:2])
-print(odd[4:])
+print(odd[:2])  #[1, 3]
+print(odd[4:])  #[9]
+
+
+a = [1, 2, 3]
+b = [4, 5, 6, 7]
+print(a+b)
+print(b+a)
+print(a * 5)
