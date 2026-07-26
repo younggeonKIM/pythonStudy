e5faf6317cd8c0938cda185aaa22313f6fc89b21
영건 김 <young3702@naver.com>
2026-07-26 16:41:27 +0900

    20260726_tupleStudy_modified

--- helloWorld/pythonBasic/tuple.py
+++ helloWorld/pythonBasic/tuple.py
@@ -2,10 +2,19 @@ t1 = ()
 t2 = (1,)
 t3 = (1, 2, 3)
 t4 = 1, 2, 3
-t5 = ('a', 'b', ('ab', 'cd'))
+t5 = ('a', 'b', ('ab', 'cd'), 6, 8)
 
 print(t1)
 print(t2)
 print(t3)
 print(t4)
 print(t5)
+
+""" t1[0] = 2       #TypeError
+del(t3[1])          #TypeError    """
+
+
+print(t5[2])
+print(t5[1:3])
+
+
